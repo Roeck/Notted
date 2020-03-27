423a333ff0131e19a2fbe6680f94f79203c0a3f0
Roeck <rons9n@gmail.com>
2020-03-27 19:24:39 -0400

    delete item functionality

--- app.js
+++ app.js
@@ -64,15 +64,25 @@ app.get( "/", function ( req, res ) {
 
 app.post( "/", function ( req, res ) {
 
-  const item = req.body.newItem;
+  const itemName = req.body.newItem;
+  const item = new Item( {
+    name: itemName
+  } );
 
-  if ( req.body.list === "Work" ) {
-    workItems.push( item );
-    res.redirect( "/work" );
-  } else {
-    items.push( item );
-    res.redirect( "/" );
-  }
+  item.save();
+  res.redirect( "/" );
+
+} );
+
+app.post( "/delete", function ( req, res ) {
+  const checkedItemId = req.body.checkbox;
+
+  Item.findByIdAndRemove( checkedItemId, function ( err ) {
+    if ( !err ) {
+      console.log( "Delete successful!" );
+      res.redirect( "/" );
+    }
+  } )
 } );
 
 app.get( "/work", function ( req, res ) {
